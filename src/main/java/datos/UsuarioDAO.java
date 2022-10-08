c52faa2eb30e8427c477dd58c4e6b090f170145d
MiguelFPP <miguelferneyp@gmail.com>
2022-10-08 17:38:58 -0500

    delete, update and select

--- src/main/java/datos/UsuarioDAO.java
+++ src/main/java/datos/UsuarioDAO.java
@@ -10,50 +10,51 @@ import java.util.List;
 import domain.Usuario;
 
 public class UsuarioDAO {
-    private static final String SQL_INSERT="INSERT INTO usuario(usuario, password) VALUES(?,?)";
-    private static final String SQL_SELECT="SELECT id, usuario, password FROM usuario WHERE usuario=? AND password=?";
-    private static final String SQL_LIST="SELECT id, usuario, password FROM usuario";
+    private static final String SQL_INSERT = "INSERT INTO usuario(usuario, password) VALUES(?,?)";
+    private static final String SQL_SELECT = "SELECT id, usuario, password FROM usuario WHERE usuario=? AND password=?";
+    private static final String SQL_LIST = "SELECT id, usuario, password FROM usuario";
+    private static final String SQL_UPDATE = "UPDATE usuario SET usuario = ?, password = ? WHERE id = ?";
+    private static final String SQL_DELETE = "DELETE FROM usuario WHERE id = ?";
 
-    public int insertar(Usuario usuario){
-        Connection conn=null;
-        PreparedStatement stmt=null;
-        int rows=0;
-        try{
-            conn=ConexionTestJV.getConecction();
-            stmt=conn.prepareStatement(SQL_INSERT);
+    public int insertar(Usuario usuario) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int rows = 0;
+        try {
+            conn = ConexionTestJV.getConecction();
+            stmt = conn.prepareStatement(SQL_INSERT);
             stmt.setString(1, usuario.getUsuario());
             stmt.setString(2, usuario.getPassword());
-            rows=stmt.executeUpdate();
-        }catch(SQLException e){
+            rows = stmt.executeUpdate();
+        } catch (SQLException e) {
             e.printStackTrace();
-        }finally{
+        } finally {
             ConexionTestJV.close(stmt);
             ConexionTestJV.close(conn);
         }
         return rows;
     }
 
-    public Usuario seleccionar(Usuario usuario){
-        Connection conn=null;
-        PreparedStatement stmt=null;
-        ResultSet rs=null;
-        Usuario usuarioObj=null;
+    public Usuario seleccionar(Usuario usuario) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Usuario usuarioObj = null;
 
         try {
-            conn=ConexionTestJV.getConecction();
-            stmt=conn.prepareStatement(SQL_SELECT);
+            conn = ConexionTestJV.getConecction();
+            stmt = conn.prepareStatement(SQL_SELECT);
             stmt.setString(1, usuario.getUsuario());
             stmt.setString(2, usuario.getPassword());
-            rs=stmt.executeQuery();
-            while(rs.next()){
-                String usuarioStr=rs.getString("usuario");
-                String password=rs.getString("password");
-                usuarioObj=new Usuario(usuarioStr, password);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                String usuarioStr = rs.getString("usuario");
+                String password = rs.getString("password");
+                usuarioObj = new Usuario(usuarioStr, password);
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        }
-        finally{
+        } finally {
             ConexionTestJV.close(rs);
             ConexionTestJV.close(stmt);
             ConexionTestJV.close(conn);
@@ -61,32 +62,99 @@ public class UsuarioDAO {
         return usuarioObj;
     }
 
-    public List<Usuario> listar(){
-        Connection conn=null;
-        PreparedStatement stmt=null;
-        ResultSet rs=null;
-        Usuario usuario=null;
-        List<Usuario> usuarios=new ArrayList<>();
+    public List<Usuario> listar() {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Usuario usuario = null;
+        List<Usuario> usuarios = new ArrayList<>();
 
         try {
-            conn=ConexionTestJV.getConecction();
-            stmt=conn.prepareStatement(SQL_LIST);
-            rs=stmt.executeQuery();
-            while(rs.next()){
-                int idUsuario=rs.getInt("id");
-                String usuarioStr=rs.getString("usuario");
-                String password=rs.getString("password");
-                usuario=new Usuario(idUsuario, usuarioStr, password);
+            conn = ConexionTestJV.getConecction();
+            stmt = conn.prepareStatement(SQL_LIST);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                int idUsuario = rs.getInt("id");
+                String usuarioStr = rs.getString("usuario");
+                String password = rs.getString("password");
+                usuario = new Usuario(idUsuario, usuarioStr, password);
                 usuarios.add(usuario);
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        }
-        finally{
+        } finally {
             ConexionTestJV.close(rs);
             ConexionTestJV.close(stmt);
             ConexionTestJV.close(conn);
         }
         return usuarios;
+    }
+
+    public Usuario selccionar(Usuario usuario) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Usuario usuarioObj = null;
+
+        try {
+            conn = ConexionTestJV.getConecction();
+            stmt = conn.prepareStatement(SQL_SELECT);
+            stmt.setString(1, usuario.getUsuario());
+            stmt.setString(2, usuario.getPassword());
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                int idUsuario = rs.getInt("id");
+                String usuarioStr = rs.getString("usuario");
+                String password = rs.getString("password");
+                usuarioObj = new Usuario(idUsuario, usuarioStr, password);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            ConexionTestJV.close(rs);
+            ConexionTestJV.close(stmt);
+            ConexionTestJV.close(conn);
+        }
+        return usuarioObj;
+    }
+
+    public int actualizar(Usuario usuario) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int rows = 0;
+
+        try {
+            conn = ConexionTestJV.getConecction();
+            stmt = conn.prepareStatement(SQL_UPDATE);
+            stmt.setString(1, usuario.getUsuario());
+            stmt.setString(2, usuario.getPassword());
+            stmt.setInt(3, usuario.getIdUsuario());
+            rows = stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            ConexionTestJV.close(stmt);
+            ConexionTestJV.close(conn);
+        }
+        return rows;
+    }
+
+    public int eliminar(Usuario usuario) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int rows = 0;
+
+        try {
+            conn = ConexionTestJV.getConecction();
+            stmt = conn.prepareStatement(SQL_DELETE);
+            stmt.setInt(1, usuario.getIdUsuario());
+            rows = stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            ConexionTestJV.close(stmt);
+            ConexionTestJV.close(conn);
+        }
+        return rows;
     }
 }
--- src/main/java/test/TestManejoUsuarios.java
+++ src/main/java/test/TestManejoUsuarios.java
@@ -5,14 +5,24 @@ import domain.Usuario;
 
 public class TestManejoUsuarios {
     public static void main(String[] args) {
-        UsuarioDAO usuarioDAO=new UsuarioDAO();
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
 
-        //Insertar un nuevo usuario
+        // Insertar un nuevo usuario
         usuarioDAO.insertar(new Usuario("Juan", "123"));
 
         /* listar usuarios */
-        for(Usuario usuario:usuarioDAO.listar()){
+        for (Usuario usuario : usuarioDAO.listar()) {
             System.out.println("usuario = " + usuario);
         }
+
+        /* seleccionar usuario */
+        Usuario usuario = usuarioDAO.seleccionar(new Usuario("Juan", "123"));
+        System.out.println("usuario = " + usuario);
+
+        /* actualizar usuario */
+        usuarioDAO.actualizar(new Usuario(1, "Juan", "123"));
+
+        /* eliminar usuario */
+        usuarioDAO.eliminar(new Usuario(1));
     }
 }
